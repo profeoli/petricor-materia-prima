1afd3b81276dd14218dc0595de9334a7b625087e
profeoli <agustinluisfeoli@gmail.com>
2026-04-11 20:24:50 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
+module.exports = nextConfig;
